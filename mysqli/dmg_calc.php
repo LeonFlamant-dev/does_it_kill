43c3db0901b48d9c454ce93374b1cc126b2f53ed
Leon Flamant <leonflamant@outlook.com>
2025-08-27 11:33:31 +0200

    added trait to stat panel

--- mysqli/dmg_calc.php
+++ mysqli/dmg_calc.php
@@ -6,6 +6,20 @@ $config = parse_ini_file("../conf.ini", true);
 // echo"mysql -h '". $config['database']['hostname'] ."' --port 3306 -p'". $config['database']['password'] ."' -u '". $config['database']['username'] ."'";
 // exit;
 
+
+switch ($_GET['palier']) {
+	case "1":
+		$attack_type = "mele";
+		break;
+	case "2":
+		$attack_type = "range";
+		break;
+	case "3":
+		$attack_type = "active";
+		break;
+}
+
+
 $mysqli = mysqli_connect($config['database']['hostname'], $config['database']['username'], $config['database']['password'], $config['database']['database']);
 if (mysqli_connect_errno()) {
 	die("Erreur connexion: " . mysqli_connect_error());
