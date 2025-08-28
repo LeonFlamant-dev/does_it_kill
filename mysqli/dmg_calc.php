94741e066092f1891b22f755f1ee51defcb26f6b
Leon Flamant <leonflamant@outlook.com>
2025-08-28 11:47:38 +0200

    added popup of dmg dealt when attacking

--- mysqli/dmg_calc.php
+++ mysqli/dmg_calc.php
@@ -24,26 +24,65 @@ switch ($_GET['attack_type']) {
 
 
 
-$mysqli = mysqli_connect($config['database']['hostname'], $config['database']['username'], $config['database']['password'], $config['database']['database']);
-if (mysqli_connect_errno()) {
-	die("Erreur connexion: " . mysqli_connect_error());
-}
+// $mysqli = mysqli_connect($config['database']['hostname'], $config['database']['username'], $config['database']['password'], $config['database']['database']);
+// if (mysqli_connect_errno()) {
+// 	die("Erreur connexion: " . mysqli_connect_error());
+// }
 
-$query="SELECT p.id, p.nom, p.nbr_hit_mele, p.perfo_mele, p.nbr_hit_range, p.perfo_range, p.item1, p.item2, p.item3, p.traits, p.faction, s.hp, s.armor, s.dmg 
-FROM personnage p 
-LEFT JOIN perso_stat s 
-ON p.nom = s.nom AND palier = '".$_GET['palier']."' 
-WHERE p.nom ='".$_GET['nom']."';";
+// $query="SELECT p.id, p.nom, p.nbr_hit_mele, p.perfo_mele, p.nbr_hit_range, p.perfo_range, p.item1, p.item2, p.item3, p.traits, p.faction 
+// FROM personnage p  
+// WHERE p.nom ='".$_GET['perso_attackant']."';";
 
 // echo $query;
 // exit;
 
-$result = mysqli_query($mysqli,$query );
+// $result = mysqli_query($mysqli,$query );
+
+// $characters = [];
+// while ($row = mysqli_fetch_assoc($result)) {
+// 	$characters = $row;
+// }
 
 
-while ($row = mysqli_fetch_assoc($result)) {
-	$characters = $row;
+
+$round = [];
+for ($i = 0; $i < 3; $i++) {
+	$thisround['round'] = $i;
+	$thisround['overkill'] = 0;
+
+	// a remplacer par une fonction marius
+	switch ($i) {
+	case 0:
+		$dmgdealt = $_GET['dmg'] * 0.8;
+		$thisround['type_of_round'] = 'lowest damage';
+		break;
+	case 1:
+		$dmgdealt = $_GET['dmg'] *1.0;
+		$thisround['type_of_round'] = 'normal damage';
+		break;
+	default:
+		$dmgdealt = $_GET['dmg'] * 1.2;
+		$thisround['type_of_round'] = 'higher damage';
+		break;
 }
+	// $dmgdealt = $dmgdealt * $characters['hit'];
+	$thisround['dmg_dealt'] = round($dmgdealt);
+
+	
+	$remainhp = $_GET['hp'] - $dmgdealt;
+
+	if ($remainhp < 0) {
+		if ($remainhp * -1 > $_GET['hp']) {
+			$thisround['overkill'] = 1;
+		}
+		$remainhp = 0;
+	}
+
+	$thisround['remain_hp'] = $remainhp;
+	$thisround['start_hp'] = $_GET['hp'];
+	$round[] = $thisround;
+}
+
 
 
 
@@ -65,7 +104,7 @@ while ($row = mysqli_fetch_assoc($result)) {
 
 
 header('Content-Type: application/json');
-echo json_encode($characters);
+echo json_encode($round);
 
-mysqli_close($mysqli);
+// mysqli_close($mysqli);
 ?>
