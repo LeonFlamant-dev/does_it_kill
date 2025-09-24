63a45f83dc5fd7a22921525882ecc17275fc810c
Leon Flamant <leonflamant@outlook.com>
2025-09-24 14:22:28 +0200

    buffs added to dmg calcul

--- mysqli/dmg_calc.php
+++ mysqli/dmg_calc.php
@@ -2,12 +2,14 @@
 
 $config = parse_ini_file("../conf.ini", true);
 
-// print_r($config);
-// echo"mysql -h '". $config['database']['hostname'] ."' --port 3306 -p'". $config['database']['password'] ."' -u '". $config['database']['username'] ."'";
-// exit;
-
-
-switch ($_GET['attack_type']) {
+// dmg_calc.php?attaque=" + encoded_atk + "&defence=" + encoded_def + "&itemsattaquant=" + encoded_item_id_1 + "&itemsdefence=" + encoded_item_id_2 + "&buffs=" + encodedBuffs
+// perso_attaque_array = [perso_attaque,attack_type,dmg_attaque,palier_attaque,faction_attaque];
+// perso_defence_array = [perso_defence,hp_defence,armor_defence,palier_defence,faction_defence];
+// items[`item_id_${i}`] = [item_selected, item_stat_1, item_stat_2];
+// buffs = [1,2,3]
+$data_perso_atk = json_decode($_GET['attaque'], true); 
+$data_perso_def = json_decode($_GET['defence'], true); 
+switch ($data_perso_def[1]) {
 	case "1":
 		$attack_type = "mele";
 		break;
@@ -19,11 +21,6 @@ switch ($_GET['attack_type']) {
 		break;
 }
 
-// attackant=" + perso_attackant + "&defenceur=" + perso_defenceur + "&attack_type=" + attack_type+ "&dmg=" + dmg_attackant+ "&hp=" + hp_defenceur+ "&armor=" + armor_defenceur
-
-
-
-
 $mysqli = mysqli_connect($config['database']['hostname'], $config['database']['username'], $config['database']['password'], $config['database']['database']);
 if (mysqli_connect_errno()) {
 	die("Erreur connexion: " . mysqli_connect_error());
@@ -33,20 +30,26 @@ if($attack_type != 'active') {
 	$query="SELECT p.id, p.nom, p.nbr_hit_".$attack_type.", p.traits, p.faction , ap.per_perfo
 	FROM personnage p  
 	left JOIN atk_perforation ap ON ap.nom = p.perfo_".$attack_type."
-	WHERE p.nom ='".$_GET['attackant']."';";
+	WHERE p.nom ='".$data_perso_atk[0]."';";
+}
+else {
+	// faire la query pour l'actif du perso
+
+
+	// ensuie parsé la reponse pour avoir les info importante
 }
 
 $result = mysqli_query($mysqli,$query );
 
-$characters = [];
+$attaque_stats = [];
 while ($row = mysqli_fetch_assoc($result)) {
-	$characters = $row;
+	$attaque_stats = $row;
 }
 
 //on recupere les items de l'attaquant
 
 
-$data = json_decode($_GET['itemsattaquant'], true); 
+$data_item_attaque = json_decode($data_perso_atk[0], true); 
 // print_r(($data));
 
 //pour le perso 1 on veux juste les chance de crit total 
@@ -55,23 +58,23 @@ $crit_chance = 0;
 $crit_value = 0;
 for ($i = 2; $i >= 0; $i--)
 {
-	print($data[$i]);
-	switch ($data['item_id_'.$i][0]) {
+	print($data_item_attaque[$i]);
+	switch ($data_item_attaque['item_id_'.$i][0]) {
 		case "crit":
 			if($crit_chance == 0) 
 			{
-				$crit_chance = ($data['item_id_'.$i][1]+$crit_boost)/100;
-				$crit_value = $crit_value+$data['item_id_'.$i][2];
+				$crit_chance = ($data_item_attaque['item_id_'.$i][1]+$crit_boost)/100;
+				$crit_value = $crit_value+$data_item_attaque['item_id_'.$i][2];
 			}
 			else
 			{
-				$crit_chance = ($data['item_id_'.$i][0]+$crit_boost)/100 + (1-($data['item_id_'.$i][1]+$crit_boost)/100)*$crit_chance;
-				$crit_value = $crit_value+$data['item_id_'.$i][2];
+				$crit_chance = ($data_item_attaque['item_id_'.$i][0]+$crit_boost)/100 + (1-($data_item_attaque['item_id_'.$i][1]+$crit_boost)/100)*$crit_chance;
+				$crit_value = $crit_value+$data_item_attaque['item_id_'.$i][2];
 			}
 			break;
 		case "crit_boost":
-			$crit_boost = $data['item_id_'.$i][1];
-			$crit_value = $crit_value+$data['item_id_'.$i][2];
+			$crit_boost = $data_item_attaque['item_id_'.$i][1];
+			$crit_value = $crit_value+$data_item_attaque['item_id_'.$i][2];
 			break;
 	}
 
@@ -79,40 +82,97 @@ for ($i = 2; $i >= 0; $i--)
 
 
 //pour le perso 2 on veux juste les stats et les chance de block total 
-$data = json_decode($_GET['itemsdefenceur'], true); 
+$data_item_def = json_decode($_GET['itemsdefenceur'], true); 
 // print_r(($data));
 $block_chance = 0;
 $block_value = 0;
-$hp = $_GET['hp']+0;
-$armor = $_GET['armor']+0;
+$hp = $data_perso_def[1]+0;
+$armor = $data_perso_def[2]+0;
 
 for ($i = 2; $i >= 0; $i--)
 {
-	switch ($data['item_id_'.$i][0]) {
+	switch ($data_item_def['item_id_'.$i][0]) {
 		case "defensive":
-			$hp = $data['item_id_'.$i][1];
-			$armor = $data['item_id_'.$i][2];
+			$hp = $data_item_def['item_id_'.$i][1];
+			$armor = $data_item_def['item_id_'.$i][2];
 			break;
 		case 'block':
-			$block_chance = ($data['item_id_'.$i][1]+$crit_boost)/100;
-			$block_value = $block_value+$data['item_id_'.$i][2];
+			$block_chance = ($data_item_def['item_id_'.$i][1]+$crit_boost)/100;
+			$block_value = $block_value+$data_item_def['item_id_'.$i][2];
 			break;
 		case "crit_boost":
-			$block_chance = $data['item_id_'.$i][1];
-			$block_value = $block_value+$data['item_id_'.$i][2];
+			$block_chance = $data_item_def['item_id_'.$i][1];
+			$block_value = $block_value+$data_item_def['item_id_'.$i][2];
 			break;
 	}
 
 }
 	
 
-// todo DMG AMPLIFICATION et MUltiplicateur
+// parser de buffs
+$nbr_hit = $attaque_stats['nbr_hit_'.$attack_type]+0;
+$dmg = $data_perso_atk[2];
 $dmg_amp = 0;
 $multiplicateur = 1;
 
+$buffs = json_decode($_GET['buffs'], true); 
+$query_buff = "SELECT b.descript, a.X, a.Y, a.Z FROM buff b LEFT JOIN ability_stat a ON b.name = a.name AND a.palier = '".$data_perso_atk[3]."' WHERE id IN (".implode(',', $buffs).")";
+$result_buff = mysqli_query($mysqli,$query_buff );
+while ($row = mysqli_fetch_assoc($result_buff)) {
+	$row['descript'] = str_replace(
+		['X', 'Y', 'Z'],
+		[$row['X'], $row['Y'], $row['Z']],
+		$row['descript']
+	);
+
+	$attaque_desc = explode(' ',$attaque_desc);
+	$indice = 0;
+	// while($indice < count($attaque_desc)) 
+	//je sais pas si on a besoin de ca vus que on fais un seul tour je sais pas si un buff peut buff plusieur fois dans ce cas oui mais comment je dif un buf de faction ou de mele d'un multibuff (new type allpersomultibuff ?)
+	// {
+		if($attaque_desc[$indice] != 'allperso' && $attaque_desc[$indice] != $data_perso_atk[4])
+		{
+			while($attaque_desc[$indice] != ',' && $indice < count($attaque_desc))
+			{
+				$indice++;
+			}
+		}
+		$indice++;
+		if($indice >= count($attaque_desc)) {break;}
+		if($attaque_desc[$indice] != 'alltype' && ($attaque_desc[$indice] != $attack_type || ($attaque_desc[$indice] == 'n_attaque' && $attack_type == 'active')))
+		{
+			while($attaque_desc[$indice] != ',' && $indice < count($attaque_desc))
+			{
+				$indice++;
+			}
+		}
+		if($indice >= count($attaque_desc)) {break;}
 
 
-$nbr_hit = $characters['nbr_hit_'.$attack_type]+0; // does nothing but help me by remaning a variable
+
+		switch ($$attaque_desc[$indice]) {
+			case "mult":
+				$multiplicateur+= $attaque_desc[$indice+1]+0; 
+				$indice+=2;
+				break;
+			case "deal":
+				$dmg+= $attaque_desc[$indice+1]+0; 
+				$indice+=2;
+				break;
+			case "take":
+				$dmg_amp+= $attaque_desc[$indice+1]+0; 
+				$indice+=2;
+				break;
+			case "add_hit":
+				$nbr_hit+= $attaque_desc[$indice+1]+0; 
+				$indice+=2;
+				break;
+			default:
+				$indice++;
+				break;
+		}
+	// }
+}
 
 $round = [];
 
@@ -129,14 +189,14 @@ for ($i = 0; $i <= $nbr_hit; $i++) {
 		// ./DoesItKill.py dmg nbr_hit per vie armor nbr_crit = 0 c_crit=0 d_crit=0 nbr_bloc=0 c_bloc = 0 v_bloc = 0 d_ampli = 0 multi = 1
 		// ./DoesItKill.py 30 2 0.3 200 20 1 0.25 12	
 
-		// echo './../DoesItKill.py '.$_GET['dmg'].' '.$nbr_hit.' '.$characters['per_perfo'].' '.$hp.' '.$armor.' '.$nbr_of_crit.' '.$crit_chance.' '.$crit_value.' '.$nbr_of_block.' '.$block_chance.' '.$block_value.' '.$dmg_amp.' '.$multiplicateur;
+		// echo './../DoesItKill.py '.$_GET['dmg'].' '.$nbr_hit.' '.$attaque_stats['per_perfo'].' '.$hp.' '.$armor.' '.$nbr_of_crit.' '.$crit_chance.' '.$crit_value.' '.$nbr_of_block.' '.$block_chance.' '.$block_value.' '.$dmg_amp.' '.$multiplicateur;
 		// exit;
-		$retour_string = shell_exec('./../DoesItKill.py '.$_GET['dmg'].' '.$nbr_hit.' '.$characters['per_perfo'].' '.$hp.' '.$armor.' '.$nbr_of_crit.' '.$crit_chance.' '.$crit_value.' '.$nbr_of_block.' '.$block_chance.' '.$block_value.' '.$dmg_amp.' '.$multiplicateur);
+		$retour_string = shell_exec('./../DoesItKill.py '.$dmg.' '.$nbr_hit.' '.$attaque_stats['per_perfo'].' '.$hp.' '.$armor.' '.$nbr_of_crit.' '.$crit_chance.' '.$crit_value.' '.$nbr_of_block.' '.$block_chance.' '.$block_value.' '.$dmg_amp.' '.$multiplicateur);
 		// { "tdmg" : [0,0,0] , "proba" : 0 }
 		// echo $retour_string;
 		$python_array = json_decode($retour_string, true);
 
-		$thisround['start_hp'] = $_GET['hp']+0;
+		$thisround['start_hp'] = $hp;
 
 		$thisround['nbr_of_crit'] = $nbr_of_crit;
 		$thisround['nbr_of_block'] = $nbr_of_block;
